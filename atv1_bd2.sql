30e85fc18a1e240ce08bcb9226a283534739c3c2
Misael <77083814+misaelorelio@users.noreply.github.com>
2023-02-15 20:23:14 -0300

    Reformulação da estrutura de dados

--- atv1_bd2.sql
+++ atv1_bd2.sql
@@ -2,46 +2,66 @@ CREATE DATABASE IF NOT EXISTS mydb;
 
 USE mydb;
 
-CREATE TABLE IF NOT EXISTS estado(
-  id INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(100) NULL,
-  sigla CHAR(2) NULL,
-  PRIMARY KEY (id));
+CREATE TABLE IF NOT EXISTS estado (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NULL,
+    sigla CHAR(2) NULL,
+    PRIMARY KEY (id)
+);
 
 
 CREATE TABLE IF NOT EXISTS cidade (
-  id INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(100) NULL,
-  estado_id INT NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_cidade_estado
-    FOREIGN KEY (estado_id)
-    REFERENCES estado (id)
-    );
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NULL,
+    estado_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_cidade_estado FOREIGN KEY (estado_id)
+        REFERENCES estado (id)
+);
+    
+CREATE TABLE IF NOT EXISTS instituicao (
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NULL,
+    PRIMARY KEY (id)
+);
 
 CREATE TABLE IF NOT EXISTS pessoa (
-  id INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(100) NULL,
-  data_nascimento DATE NULL,
-  cidade_id INT NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_pessoa_cidade1
-    FOREIGN KEY (cidade_id)
-    REFERENCES cidade (id)
-  );
-
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NULL,
+    data_nascimento DATE NULL,
+    instituicao_id INT NOT NULL,
+    cidade_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_pessoa_cidade1 FOREIGN KEY (cidade_id)
+        REFERENCES cidade (id),
+    CONSTRAINT fk_pessoa_instituicao FOREIGN KEY (instituicao_id)
+        REFERENCES instituicao (id)
+);
+  
 CREATE TABLE IF NOT EXISTS curso (
-  id INT NOT NULL AUTO_INCREMENT,
-  nome VARCHAR(100) NULL,
-  professor VARCHAR(100) NULL,
-  curso VARCHAR(45) NULL,
-  pessoa_id INT NOT NULL,
-  PRIMARY KEY (id),
-  CONSTRAINT fk_curso_pessoa1
-    FOREIGN KEY (pessoa_id)
-    REFERENCES pessoa (id)
-   );
+    id INT NOT NULL AUTO_INCREMENT,
+    nome VARCHAR(100) NULL,
+    professor VARCHAR(100) NULL,
+    instituicao_id INT NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_curso_instituicao FOREIGN KEY (instituicao_id)
+        REFERENCES instituicao (id)
+);
+  
+CREATE TABLE IF NOT EXISTS pessoa_curso (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    curso_id INT NOT NULL,
+    pessoa_id INT NOT NULL,
+    CONSTRAINT fk_curso FOREIGN KEY (curso_id)
+        REFERENCES curso (id),
+    CONSTRAINT fk_pessoa FOREIGN KEY (pessoa_id)
+        REFERENCES pessoa (id)
+);
 
+
+
+   
 INSERT INTO estado(nome, sigla)
 VALUES ('Paraná','PR');
 
